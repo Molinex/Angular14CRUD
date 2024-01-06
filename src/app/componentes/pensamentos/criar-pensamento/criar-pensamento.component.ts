b1f32ace968987d00ecfcd25ebdee546bb6c3652
Molina <elison_li@hotmail.com>
2024-01-06 16:01:54 -0300

    contrucao parte visual e fluxo de dados

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert("Botão SALVAR funcionando!!!!")
+  }
+  cancelar(){
+    alert("Botão CANCELAR funcionando!!!!")
+  }
+
+}
