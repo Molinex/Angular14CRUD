326416947b5a3098ae323120c1136221fb16e5b5
Molina <elison_li@hotmail.com>
2024-01-10 14:31:53 -0300

    Configurando Mural

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamento = [
+    {
+      conteudo: 'Pegando valor da Lista 3',
+      autoria: 'Molinox supra 3',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Pegando valor da Lista 2',
+      autoria: 'Molinox supra 2',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Pegando valor da Lista 3  Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3 Pegando valor da Lista 3',
+      autoria: 'Molinox supra 3',
+      modelo: 'modelo2'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
